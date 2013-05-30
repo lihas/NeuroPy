45fba0e27237ce16a42cfe7cc64c5131495b64f9
lihas <lihashgnis@yahoo.in>
2013-05-30 19:56:10 +0530

    added source files and setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+ 
+setup(name = "NeuroPy",
+      version = "0.1",
+      description = "Library for interfacing with Neurosky's Mindwave EEG headset",
+      author = "sahil singh",
+      author_email = "lihashgnis@yahoo.in",
+      url = "https://github.com/lihas/NeuroPy",
+      packages=["NeuroPy"]
+      )
